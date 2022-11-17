fab4da7d9bf62c845503d9ebcc784e2a33f006da
ashutoshkr-sc <ashutoshkumar@sharechat.co>
2022-11-17 23:30:38 +0530

    navbar added router added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import AdminUpload from './pages/AdminUpload';
 import Landing from './pages/Landing';
 import VideoLogs from './pages/VideoLogs'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
+       
         <Route path='/' element={
           <>
             <Landing/>
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,10 +3,20 @@ function Landing() {
     return (
         <div className='p-8'>
             
-            <div className='w-full rounded-xl flex bg-gradient-to-br from-blue-500 to-cyan-500' style={{height: "50vh"}}>
-                <h1 className='font-bold text-xl p-4'>Pathshala By ShareChat.</h1>
+            <div className='w-full items-center rounded-xl flex bg-gradient-to-br from-blue-500 to-cyan-500' style={{height: "50vh"}}>
+                <div className='w-1/2 p-4'>
+                    <h1 className=''>Educating Engineers</h1>
+                    <h1 className='pt-4 font-bold text-4xl'>Pathshala By ShareChat. Insider Updates on Our Latest Projects. </h1>
+                    <h1 className='pt-8'>We bring you the best from the world of design, tech and latest apps with an insight of our successful projects and their stories</h1>
+                    <div className='flex pt-4'>
+                        <div className='bg-[#2C3333] drop-shadow-xl text-white font-bold rounded-xl py-3 px-8 m-2'>Videos</div>
+                        <div className='bg-[#2C3333] drop-shadow-xl text-white font-bold rounded-xl py-3 px-8 m-2'>Blogs</div>
+                    </div>
+                </div>
+                
+                
             </div>
-            <img className="w-5/12 h-full absolute top-0 right-0" src="/educator.png" alt="Educator logo"/>
+            <img className="w-5/12 absolute top-0 right-0" src="/educator.png" alt="Educator logo"/>
         </div>
     )
 }
