ccaf7e37f33e95e3b3a8d60f6b7d12ef065246f2
Ankush-Gupta-DP-08 <ankushgupta@sharechat.co>
2022-11-17 17:01:32 +0530

    Added Tittle Cards, Basic List Layout

--- src/pages/VideoLogs.jsx
+++ src/pages/VideoLogs.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import Option from '../components/Option'
+import './videoLogs.css'
 function VideoLogs() {
+    const [titles, setTitles] = useState([
+        "Engineering",
+        "Artificial Intelligence",
+        "Product",
+        "Design",
+        "Content & Creators"
+    ])
+    let displayAllTitles, displayContent;
+    const [optionSelected, setSelectedoption] = useState('Engineering');
+    const [content, setContent] = useState([
+        {
+            title: `title-1 + ${optionSelected}`,
+            author: `author-1 + ${optionSelected}`
+        },
+        {
+            title: `title-2 + ${optionSelected}`,
+            author: `author-2 + ${optionSelected}`
+        }
+    ]);
+    useEffect(() => {
+        setContent([
+            {
+                title: `title-1 + ${optionSelected}`,
+                author: `author-1 + ${optionSelected}`
+            },
+            {
+                title: `title-2 + ${optionSelected}`,
+                author: `author-2 + ${optionSelected}`
+            }
+        ])
+    }, [optionSelected])
+    displayAllTitles = titles.map((title, index) => {
+        return (
+            <Option key={index} title={title} setSelectedoption={setSelectedoption} />
+        )
+    })
+    displayContent = content.map((con, index) => {
+        return (
+            <div key={index}>
+                <h1>{con.title}</h1>
+                <h2>{con.author}</h2>
+            </div>
+        )
+    })
     return (
         <div>
-            Video Logs
+            <div className='contianer'>{displayAllTitles}</div>
+            <div>{displayContent}</div>
         </div>
     )
 }
