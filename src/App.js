d841b5dfae5b027f10eefbebca5900a37e71740d
Ankush-Gupta-DP-08 <ankushgupta@sharechat.co>
2022-11-17 15:11:24 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Landing from './pages/Landing';
+import VideoLogs from './pages/VideoLogs';
 
 function App() {
   return (
     <div className="App">
-      <Landing />
+      <VideoLogs />
     </div>
   );
 }
---
+++ src/pages/VideoLogs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function VideoLogs() {
+    return (
+        <div>
+            Video Logs
+        </div>
+    )
+}
+
+export default VideoLogs
